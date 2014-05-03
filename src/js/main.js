2b6d96968d81c38070725ad00ab251440e89adf7
kerry95 <95kerry@gmail.com>
2014-05-03 22:17:54 +0800

    Achieve playing live video, recording video and download recorded video

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,108 @@
-(function(window){
-    function query(selector){
-        return window.document.querySelectorAll(selector) || null;
-    }
+// ;(function(window){
 
-    // variables init
-    var selfVideo = query('#selfVideo'),
-        startBtn = query('#startBtn');
+// variables init
+var document = window.document
+  , $ = window.$
 
-    var handlers = {
-        onClickStartBtn: function(){
-            selfVideo.autoPlay = true;
-            navigator.getUserMedia(
-                {
-                    video: {
-                        mandatory: {
-                            minWidth: 640,
-                            minHeight: 480
-                        },
-                        optional: [{
-                            minFrameRate: 30
-                        }]
-                    }
-                },
-                handlers.onGetUserMediaSuccess,
-                handlers.onGetUserMediaFail
-            );
-        },
-        onGetUserMediaSuccess: function(stream){
-            selfVideo.src = window.URL.createObjectURL(stream);
-        },
-        onGetUserMediaFail: function(event){
-            console.error('Getting user media fails: %O', event);
+var $liveBox = $('#liveBox')
+  , $recordBox = $('#recordBox')
+  , $resolutionBox = $('#resolutionBox')
+  , $recordDurationBox = $('#recordDuration')
+  , $recordDuration = $('#recordDuration')
+  , $liveVideo = $('#liveVideo')
+  , liveVideo = $liveVideo.get(0)
+  , $openCameraBtn = $('#openCameraBtn')
+  , $finishRecordBtn = $('#finishRecordBtn')
+  , $recordBtn = $('#recordBtn')
+  , $resolution = $('#resolution')
+  , $recordVideo = $('#recordVideo')
+  , recordVideo = $recordVideo.get(0)
+  , $recordVideoLink = $('#recordVideoLink')
+  , recordVideoLink = $recordVideoLink.get(0)
+  , liveVideoRecorder
+  , recordStartTime
+  , recordTimerId = null
+
+var handlers = {
+  onSelfVideoPlay: function(){
+    var width, height
+    width = liveVideo.width = liveVideo.videoWidth
+    height = liveVideo.height = liveVideo.videoHeight
+    // TODO: display resolution, considering enough factors
+    $resolution.text(width + 'x' + height + 'px')
+  },
+  onClickOpenCameraBtn: function(){
+    navigator.getUserMedia(
+      {
+        video: {
+          mandatory: {
+            minWidth: 640,
+            minHeight: 480
+          },
+          optional: [{
+            minFrameRate: 30
+          }]
         }
-    };
+      },
+      handlers.onGetUserMediaSuccess,
+      handlers.onGetUserMediaFail
+    )
+  },
+  onGetUserMediaSuccess: function(stream){
+    $liveBox.show()
+    liveVideo.src = window.URL.createObjectURL(stream)
+  },
+  onGetUserMediaFail: function(event){
+    alert('Get user media fails')
+    console.error('Get user media fails: %O', event)
+  },
+  onClickRecordBtn: function(event){
+    liveVideoRecorder = new VideoRecorder(liveVideo)
+    recordStartTime = liveVideoRecorder.start()
 
-    // polyfill
-    navigator.getUserMedia =
-        navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia;
+    handlers.updateRecordDuration()
+    recordTimerId = setInterval(handlers.updateRecordDuration, 1000)
+    $recordDurationBox.show()
+  },
+  onClickFinishRecordBtn: function(event){
+    var objectUrl, record, webmBlob, fps
 
-    startBtn.addEventListener('click', handlers.onClickStartBtn);
-})(this);
+    clearInterval(recordTimerId)
+
+    record = liveVideoRecorder.finish()
+
+    handlers.updateRecordDuration(record.duration)
+    recordVideo.width = liveVideo.width
+    recordVideo.height = liveVideo.height
+
+    fps = Math.round( record.frames.length / (record.duration / 1000) )
+    webmBlob = Whammy.fromImageArray(record.frames, fps)
+    objectUrl = window.URL.createObjectURL( webmBlob )
+    recordVideo.src = objectUrl
+
+    recordVideoLink.href = objectUrl
+
+    $recordBox.show()
+
+    console.log('%d fps: %d frames in %d ms', fps, record.frames.length, record.duration)
+  },
+  updateRecordDuration: function(duration){
+    duration = duration !== undefined ? duration : Date.now() - recordStartTime
+    $recordDuration.text( parseInt(duration / 1000) + 's' )
+  }
+}
+
+liveVideo.autoplay = true
+
+recordVideo.autoplay = true
+recordVideo.controls = true
+recordVideo.loop = true
+
+recordVideoLink.download = 'record.webm'
+
+$liveVideo.on('play', handlers.onSelfVideoPlay)
+$openCameraBtn.on('click', handlers.onClickOpenCameraBtn)
+$recordBtn.on('click', handlers.onClickRecordBtn)
+$finishRecordBtn.on('click', handlers.onClickFinishRecordBtn)
+
+// })(this)
---
+++ src/js/polyfill.js
@@ -0,0 +1,23 @@
+// polyfill
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia
+
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.msRequestAnimationFrame
+
+window.cancelAnimationFrame =
+  window.cancelAnimationFrame ||
+  window.webkitCancelAnimationFrame ||
+  window.mozCancelAnimationFrame ||
+  window.msCancelAnimationFrame
+
+window.URL =
+  window.URL ||
+  window.webkitURL ||
+  window.mozURL ||
+  window.msURL
